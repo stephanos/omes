00d7d119a111cb3f03fae25cc5ee94c8ba609076
Stephan Behnke <stephan.behnke@temporal.io>
2024-11-18 12:25:53 -0800

    rename

--- cmd/run_scenario.go
+++ cmd/run_scenario.go
@@ -144,11 +144,11 @@ func (r *scenarioRunner) run(ctx context.Context) error {
 		Namespace:       r.clientOptions.Namespace,
 		RootPath:        rootDir(),
 	}
-	assert.Sometimes(true, "[wkl] Omes scenario",
+	assert.Sometimes(true, "[WKL] Omes scenario",
 		map[string]any{"scenario": scenarioInfo.ScenarioName})
 	err = scenario.Executor.Run(ctx, scenarioInfo)
 	if err != nil {
-		assert.Unreachable("[wkl] Omes scenario failed", map[string]any{"err": err})
+		assert.Unreachable("[WKL] Omes scenario failed", map[string]any{"err": err})
 		return fmt.Errorf("failed scenario: %w", err)
 	}
 	return nil
